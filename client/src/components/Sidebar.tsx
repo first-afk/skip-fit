4aa7a467d509b3653f70a258bad4257c6b7f2b5a
first-afk <oriejiestherr@gmail.com>
2026-06-01 12:37:04 +0100

    minor fixes

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -14,7 +14,7 @@ const Sidebar = () => {
   return (
     <nav className="hidden lg:flex flex-col w-64 bg-whit dark:bg-slate-900 border-r border-slate-100 dark:border-slate-800 p-6 transition-colors duration-200">
       <div className="flex items-center gap-3 mb-8">
-        <div className="size-10 rounded-xl bg-emerald-500 flex items-center">
+        <div className="size-10 rounded-xl bg-emerald-500 flex justify-center items-center">
           <PersonStanding className="size-7 text-white" />
         </div>
         <h1 className="text-2xl font-bold text-slate-800 dark:text-white">
--- client/src/pages/Activity.tsx
+++ client/src/pages/Activity.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
