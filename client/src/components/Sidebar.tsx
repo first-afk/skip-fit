688dbb820359749705e42798e70f2fa5760ae981
first-afk <oriejiestherr@gmail.com>
2026-07-16 19:33:23 +0100

    chore: bug fix

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -11,7 +11,7 @@ const Sidebar = () => {
 
   // const { theme, toggleTheme } = useTheme();
   return (
-    <nav className="hidden lg:flex flex-col w-64 bg-whit dark:bg-slate-900 border-r border-slate-100 dark:border-slate-800 p-6 transition-colors duration-200">
+    <nav className="hidden lg:flex flex-col w-64 bg-white dark:bg-slate-900 border-r border-slate-100 dark:border-slate-800 p-6 transition-colors duration-200">
       <div className="flex items-center gap-3 mb-8">
         <div className="size-10 rounded-xl bg-emerald-500 flex justify-center items-center">
           <PersonStanding className="size-7 text-white" />
